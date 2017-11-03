d8536047934eca495027c4ff69bb9b256f177426
itsnhduc <nhduc.lhp@gmail.com>
2017-11-03 16:19:23 +0800

    Add paragraph

--- src/ReuseMe.js
+++ src/ReuseMe.js
@@ -2,7 +2,8 @@ export default ReuseMe = () => {
   return (
     <div>
       <div>You have successful reused a React component.</div>
-      <p>This component has 1 paragraph.</p>
+      <p>This component has 2 paragraph now.</p>
+      <p>Hi!</p>
     </div>
   )
 }
