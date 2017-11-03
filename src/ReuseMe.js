a353f6f66645bc2e0dbd6a3e7130d523e7f5dfd1
itsnhduc <nhduc.lhp@gmail.com>
2017-11-03 16:27:55 +0800

    Revert stuff

--- src/ReuseMe.js
+++ src/ReuseMe.js
@@ -1,9 +1,5 @@
 export default ReuseMe = () => {
   return (
-    <div>
-      <div>You have successful reused a React component.</div>
-      <p>This component has 2 paragraph now.</p>
-      <p>Hi!</p>
-    </div>
+    <div>Changing stuff</div>
   )
 }
